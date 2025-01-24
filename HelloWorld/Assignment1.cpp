0e940a72039298659a1d1f7b1c323f35c935f529
Mizuk1nam <150742190+Mizuk1nam@users.noreply.github.com>
2025-01-23 20:33:38 -0500

    finished assignment

--- HelloWorld/Assignment1.cpp
+++ HelloWorld/Assignment1.cpp
@@ -2,9 +2,10 @@
 #include <string>
 
 class UserAccount {
+public:
     int ID;                
     std::string userName;   
-    const std::string password;   
+    std::string password;   
 
     UserAccount(int id, const std::string& name, const std::string& pass)
         : ID(id), userName(name), password(pass) {
@@ -14,3 +15,40 @@ class UserAccount {
     }
     
 };
+int main() {
+    int id;
+    std::string username, password;
+
+    std::cout << "Enter account ID: ";
+    std::cin >> id;
+    std::cin.ignore();
+
+    std::cout << "Enter username: ";
+    std::getline(std::cin, username);
+
+    std::cout << "Enter password: ";
+    std::getline(std::cin, password);
+
+    UserAccount account(id, username, password);
+
+    std::string enteredUsername, enteredPassword;
+
+    while (true) {
+        std::cout << "\nLogin\n";
+        std::cout << "Enter username: ";
+        std::getline(std::cin, enteredUsername);
+
+        std::cout << "Enter password: ";
+        std::getline(std::cin, enteredPassword);
+
+        if (account.Login(enteredUsername, enteredPassword)) {
+            std::cout << "Login successful! Account ID: " << account.ID << std::endl;
+            break;
+        }
+        else {
+            std::cout << "Invalid username or password. Try again." << std::endl;
+        }
+    }
+
+    return 0;
+}
