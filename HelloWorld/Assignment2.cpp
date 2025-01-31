966f47021e147df1e461b738829221b0f5df6aca
Mizuk1nam <150742190+Mizuk1nam@users.noreply.github.com>
2025-01-30 20:47:18 -0500

    Started pointers assignment

---
+++ HelloWorld/Assignment2.cpp
@@ -0,0 +1,96 @@
+﻿/* 
+Pointers assignment
+Question 1
+int a = 10;
+int* p = &a;
+*p = 20;
+std::cout << a;
+​
+Questions:
+What will be the output?
+20
+Will this run/compile?
+Yes
+Is there a memory leak or dangling pointer?
+None
+
+Question 2
+int* p = nullptr;
+*p = 5;
+​
+Questions:
+What will be the output?
+No output
+Will this run/compile?
+It would complie but not run
+Is there a memory leak or dangling pointer?
+None
+
+## Question 2.1
+
+```cpp
+int a = 5;
+int* p = &a;
+int b = *p;
+*p = 10;
+std::cout << a << " " << b;
+```
+
+**Questions**:
+
+- What will be the output?
+10 5
+- Will this run/compile?
+Yes to both
+- Is there a memory leak or dangling pointer?
+No to both
+
+
+## Question 3
+
+```cpp
+int* p = new int(5);
+delete p;
+*p = 10;
+```
+
+**Questions**:
+
+- What will be the output?
+None
+- Will this run/compile?
+Would compile but not run
+- Is there a memory leak or dangling pointer?
+No memory leak but p is a dangling pointer
+
+## Question 4
+
+```cpp
+int a = 10;
+float* p = (float*)&a;
+std::cout << *p;
+```
+
+**Questions**:
+
+- What will be the output?
+The output might not be 10 because its converting int to float
+- Will this run/compile?
+It would compile but not run
+- Is there a memory leak or dangling pointer?
+None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
